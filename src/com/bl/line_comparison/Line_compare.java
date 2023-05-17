8ec025fd592462cb190fdbc3b4867ee1c5d36f54
vik554 <kumbharvikram27@gmail.com>
2023-05-17 16:10:58 +0530

    ADDED:wlcome masg to line comparison programe

---
+++ src/com/bl/line_comparison/Line_compare.java
@@ -0,0 +1,7 @@
+package com.bl.line_comparison;
+
+public class Line_compare {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Programme");
+    }
+}
