912dc61fcc047deb8f2f5af112001a737d7c9b1d
vik554 <kumbharvikram27@gmail.com>
2023-05-18 00:45:03 +0530

    ADDED: code to uc2_line_comparison

--- src/com/bl/line_comparison/Line_compare.java
+++ src/com/bl/line_comparison/Line_compare.java
@@ -1,5 +1,6 @@
 package com.bl.line_comparison;
 import java.util.Scanner;
+import java.util.Objects;
 
 public class Line_compare {
     public static void main(String[] args) {
@@ -18,7 +19,27 @@ public class Line_compare {
         double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(Y2 - y1, 2));
 
         System.out.println("The Length of Line Is: " + length);
+        //for uc2
+        System.out.print("Enter X1 Value: ");
+        double a1 = ab.nextDouble();
+        System.out.print("Enter Y1 Value: ");
+        double b1 = ab.nextDouble();
+        System.out.print("Enter X2 Value: ");
+        double a2 = ab.nextDouble();
+        System.out.print("Enter Y2 Value: ");
+        double b2 = ab.nextDouble();
 
+        /* Calculating the Line 2 Length UC-2 */
+
+        double length2 = Math.sqrt((Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2)));
+
+        System.out.println("The Length of Line 2 is: " + length2);
+
+        if (Objects.equals(length, length2)) {
+            System.out.println("Line 1 is equal to line 2.");
+        } else {
+            System.out.println("Line 1 is not equal to line 2.");
+
+        }
     }
-
 }
